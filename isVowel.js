92cebc1472400917efe116b4461539b5a888e4be
BellaAguirre <yunoshe1@gmail.com>
2018-06-05 20:37:29 -0500

    primer commit

---
+++ isVowel.js
@@ -0,0 +1,4 @@
+const isVowel = function(char){
+    return ['a','e','i','o','u'].indexOf(char) > -1;
+};
+module.exports = isVowel;
---
+++ isVowel.spec.js
@@ -0,0 +1,70 @@
+/*const isVowel = require('./isVowel');
+console.log('deberia devolver true para letra a');
+if(isVowel('a') === true){
+    console.log('✓ ok');
+}else{
+    console.error('✗ fail');
+}
+
+console.log('deberia devolver false para letra b');
+if(isVowel('b') === false){
+    console.log('✓ ok');
+}else{
+    console.error('✗ fail');
+}*/
+/*
+const isVowel = require('./isVowel');
+describe('isVowel()',()=>{
+    it('deberia devolver true para letra a',()=>{
+        if(isVowel('a') === true){
+            console.log('✓ ok');
+        }else{
+            console.error('✗ fail');
+        }
+    });
+    it('deberia devolver false para letra b',()=>{
+        if(isVowel('b') === false){
+            console.log('✓ ok');
+        }else{
+            console.error('✗ fail');
+        }
+    });
+
+});*/
+/*const isVowel =require('./isVowel');
+const assertEqual =(value, expected) =>{
+    if(value!==expected){
+        throw new Error(`Expected '${value}' to equal '${expected}'`);
+    }
+};
+ describe('isVowel()',()=>{
+    it('deberia devolver true para letra a',()=>{
+        assertEqual(isVowel('a'),true);
+    });
+    it('deberia devolver false para letra b',()=>{
+        assertEqual(isVowel('b'),false);
+    });
+ });*/
+ /*const assert =require('assert');
+ const isVowel =require('./isVowel');
+
+ describe('isVowel()',()=>{
+    it('deberia devolver true para letra a',()=>{
+        assert.equal(isVowel('a'),true);
+    });
+    it('deberia devolver false para letra b',()=>{
+        assert.equal(isVowel('b'),false);
+    });
+ });*/
+ /*const {assert} =require('chai');
+ const isVowel =require('./isVowel');
+
+ describe('isVowel()',()=>{
+    it('deberia devolver true para letra a',()=>{
+        assert.equal(isVowel('a'),true);
+    });
+    it('deberia devolver false para letra b',()=>{
+        assert.equal(isVowel('b'),false);
+    });
+ });*/
+ 
